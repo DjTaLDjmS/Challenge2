37827ba65b02243c1f9791ac70eb374c30a1dcdd
DjTaLDjmS <sdemarco@hotmail.fr>
2023-01-11 00:06:29 +0100

    Challenge 2 - Play Fetch - 8.Bonus: The spawn interval is always the same

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,24 +10,37 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
+    //private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        spawnInterval = RetrunSpawnInterval();
+        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        StartCoroutine(SpawnRandomBall());
+    }
+    
+    //Return a spawn interval
+    float RetrunSpawnInterval()
+    {
+        return Random.Range(3.0f, 5.0f);
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    IEnumerator SpawnRandomBall()
     {
-        // Generate random ball index and random spawn position
-        int ballIndex = Random.Range(0, ballPrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        while (true)
+        {
+            spawnInterval = RetrunSpawnInterval();
+            int ballIndex = Random.Range(0, ballPrefabs.Length);
+            // Generate random ball index and random spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+            // instantiate ball at random spawn location
+            Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+
     }
-
 }
