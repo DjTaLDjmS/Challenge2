097f32ac3ef27c7e19486ee7c54b81d39a9cccaa
DjTaLDjmS <sdemarco@hotmail.fr>
2023-01-11 00:08:55 +0100

    Challenge 2 - Play Fetch - 9.Bonus: The player can “spam” the spacebar key

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private DateTime time;
+
+    [SerializeField]
+    private int delay = 500;
+
+    private void Start()
+    {
+        time = DateTime.Now;
+    }
 
     // Update is called once per frame
     void Update()
@@ -12,7 +22,11 @@ public class PlayerControllerX : MonoBehaviour
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if ((DateTime.Now - time).TotalMilliseconds > delay)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                time = DateTime.Now;
+            }
         }
     }
 }
